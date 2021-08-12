0b26b5a5befb547c4c72bc0fa33b08823d64b324
Keval Navadiya <kevalnavadiya39@gmail.com>
2021-08-12 21:34:39 +0530

    bcryptjs installed

--- server/apis/user.js
+++ server/apis/user.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 //------------------------------------------------------------------------
 
@@ -17,121 +17,121 @@ const passRound = process.env.PASS_ROUND;
 //------------------------------------------------------------------------
 
 const loginUser = async (req, res) => {
-	try {
-		// get username and password
-		const { username, password } = req.body;
+  try {
+    // get username and password
+    const { username, password } = req.body;
 
-		if (!username || !password) {
-			return res
-				.status(resConfig.BAD_REQUEST.status)
-				.send(resConfig.BAD_REQUEST);
-		}
+    if (!username || !password) {
+      return res
+        .status(resConfig.BAD_REQUEST.status)
+        .send(resConfig.BAD_REQUEST);
+    }
 
-		let comparedPassword = "";
-		let userType = "";
-		let firstTime = "";
-		let blocked = "";
-		let balance = null;
+    let comparedPassword = "";
+    let userType = "";
+    let firstTime = "";
+    let blocked = "";
+    let balance = null;
 
-		// if user type is user
-		const user = await User.findOne({ refNo: username })
-			.select(["refNo", "password", "balance"])
-			.lean();
+    // if user type is user
+    const user = await User.findOne({ refNo: username })
+      .select(["refNo", "password", "balance"])
+      .lean();
 
-		if (user) {
-			comparedPassword = user.password;
-			userType = "user";
-			balance = user.balance;
-		}
+    if (user) {
+      comparedPassword = user.password;
+      userType = "user";
+      balance = user.balance;
+    }
 
-		// if userType is merchant
-		if (!comparedPassword) {
-			const merchant = await Merchant.findOne({ username })
-				.select(["username", "password", "blocked", "firstTime"])
-				.lean();
+    // if userType is merchant
+    if (!comparedPassword) {
+      const merchant = await Merchant.findOne({ username })
+        .select(["username", "password", "blocked", "firstTime"])
+        .lean();
 
-			if (merchant) {
-				comparedPassword = merchant.password;
-				firstTime = merchant.firstTime;
-				blocked = merchant.blocked;
-				userType = "merchant";
-			}
-		}
+      if (merchant) {
+        comparedPassword = merchant.password;
+        firstTime = merchant.firstTime;
+        blocked = merchant.blocked;
+        userType = "merchant";
+      }
+    }
 
-		// check the password
-		if (
-			!comparedPassword ||
-			!(await bcrypt.compare(password, comparedPassword))
-		) {
-			return res
-				.status(resConfig.UNAUTHORIZED.status)
-				.send(resConfig.UNAUTHORIZED);
-		}
+    // check the password
+    if (
+      !comparedPassword ||
+      !(await bcrypt.compare(password, comparedPassword))
+    ) {
+      return res
+        .status(resConfig.UNAUTHORIZED.status)
+        .send(resConfig.UNAUTHORIZED);
+    }
 
-		const payLoad = { username, userType, blocked };
+    const payLoad = { username, userType, blocked };
 
-		// generate token for the user
-		const token = cryptoUtils.encodeJwt(payLoad, jwtKey, authTokenDuration);
+    // generate token for the user
+    const token = cryptoUtils.encodeJwt(payLoad, jwtKey, authTokenDuration);
 
-		const resPayload = { ...resConfig.SUCCESS };
-		resPayload.token = token;
-		resPayload.username = username;
-		resPayload.userType = userType;
-		resPayload.blocked = blocked;
-		resPayload.firstTime = firstTime;
-		resPayload.balance = balance;
+    const resPayload = { ...resConfig.SUCCESS };
+    resPayload.token = token;
+    resPayload.username = username;
+    resPayload.userType = userType;
+    resPayload.blocked = blocked;
+    resPayload.firstTime = firstTime;
+    resPayload.balance = balance;
 
-		return res.status(resConfig.SUCCESS.status).send(resPayload);
-	} catch (error) {
-		console.log("error in loginUser", error);
-		return res
-			.status(resConfig.SERVER_ERROR.status)
-			.send(resConfig.SERVER_ERROR);
-	}
+    return res.status(resConfig.SUCCESS.status).send(resPayload);
+  } catch (error) {
+    console.log("error in loginUser", error);
+    return res
+      .status(resConfig.SERVER_ERROR.status)
+      .send(resConfig.SERVER_ERROR);
+  }
 };
 
 const createMerchant = async (req, res) => {
-	try {
-		// get username and password
-		const { email } = req.body;
+  try {
+    // get username and password
+    const { email } = req.body;
 
-		if (!email) {
-			return res
-				.status(resConfig.BAD_REQUEST.status)
-				.send(resConfig.BAD_REQUEST);
-		}
+    if (!email) {
+      return res
+        .status(resConfig.BAD_REQUEST.status)
+        .send(resConfig.BAD_REQUEST);
+    }
 
-		// generate username
-		let username = `DBH_`;
+    // generate username
+    let username = `DBH_`;
 
-		for (let i = 0; i < 6; i++) {
-			username += Math.ceil(Math.random() * 10);
-		}
+    for (let i = 0; i < 6; i++) {
+      username += Math.ceil(Math.random() * 10);
+    }
 
-		// generate random password
-		const password = cryptoUtils.generateRandomPassword(10);
+    // generate random password
+    const password = cryptoUtils.generateRandomPassword(10);
 
-		const hashedPassword = await bcrypt.hash(password, parseInt(passRound));
+    const hashedPassword = await bcrypt.hash(password, parseInt(passRound));
 
-		// create merchant
-		const MERCHANT = new Merchant({
-			username,
-			password: hashedPassword,
-			email,
-		});
+    // create merchant
+    const MERCHANT = new Merchant({
+      username,
+      password: hashedPassword,
+      email,
+    });
 
-		await MERCHANT.save();
+    await MERCHANT.save();
 
-		// send username and password to merchant
-		console.log("---", username, password, email);
+    // send username and password to merchant
+    console.log("---", username, password, email);
 
-		return res.status(resConfig.SUCCESS.status).send(resConfig.SUCCESS);
-	} catch (error) {
-		console.log("error in createMerchant", error);
-		return res
-			.status(resConfig.SERVER_ERROR.status)
-			.send(resConfig.SERVER_ERROR);
-	}
+    return res.status(resConfig.SUCCESS.status).send(resConfig.SUCCESS);
+  } catch (error) {
+    console.log("error in createMerchant", error);
+    return res
+      .status(resConfig.SERVER_ERROR.status)
+      .send(resConfig.SERVER_ERROR);
+  }
 };
 //------------------------------------------------------------------------
 
