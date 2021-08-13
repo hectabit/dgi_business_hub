07b2eb0daeccb043248c3ce2e9536919540bfc8e
Keval Navadiya <kevalnavadiya39@gmail.com>
2021-08-13 14:33:09 +0530

    express file limit added

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ const env = process.env.ENV || "";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+// app.use(express.json());
 
 app.use("/api/user", userRouters);
 app.use("/api/ad", adRouters);
