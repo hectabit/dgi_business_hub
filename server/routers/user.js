fadae9fffc7d1dc2e23b43f4969aac5da674cf8b
yash khokhaneshiya <yash.khokhaneshiya@artoon.in>
2021-08-23 21:37:02 +0530

    user getDetails route added

--- server/routers/user.js
+++ server/routers/user.js
@@ -4,15 +4,16 @@ const router = express.Router();
 //------------------------------------------------------------------------
 
 const {
-  loginUser,
-  createMerchant,
-  scanCode,
-  getGiftCards,
-  getAllMerchant,
-  getMerchantDetailsById,
-  approveMerchantById,
-  disapproveMerchantById,
-  changeMerchantPassword,
+	loginUser,
+	createMerchant,
+	scanCode,
+	getGiftCards,
+	getAllMerchant,
+	getMerchantDetailsById,
+	approveMerchantById,
+	disapproveMerchantById,
+	changeMerchantPassword,
+	getUserDetails,
 } = require("../apis/user");
 const { isUserAdmin, isUserThere } = require("../middleware");
 
@@ -26,11 +27,12 @@ router.get("/all", getAllMerchant);
 router.get("/", getMerchantDetailsById);
 router.get("/approve", approveMerchantById);
 router.get("/disapprove", disapproveMerchantById);
+router.get("/details", getUserDetails);
 router.post("/login", loginUser);
 router.post(
-  "/create",
-  // isUserAdmin,
-  createMerchant
+	"/create",
+	// isUserAdmin,
+	createMerchant
 );
 router.post("/scan", isUserThere, scanCode);
 router.post("/changePassword", changeMerchantPassword);
